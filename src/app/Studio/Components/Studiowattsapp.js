acb03a469bcd5a2ed4ca12a672588e5957732506
Muzamil Ali <muzamil_ali92@yahoo.com>
2025-11-05 17:43:34 +0500

    'aisy'

--- src/app/Studio/Components/Studiowattsapp.js
+++ src/app/Studio/Components/Studiowattsapp.js
@@ -5,7 +5,7 @@ export default function StudioFloatingbutton() {
   return (
     <div>
       <Link 
-        href="https://wa.me/+923234464400" // Replace with your actual number in international format
+        href="https://wa.me/+92234464400"// Replace with your actual number in international format
         aria-label="Chat on WhatsApp"
         className="fixed bottom-6 right-6 bg-green-600 text-white p-4 rounded-full shadow-lg z-50 transition duration-300"
         target="_blank"
